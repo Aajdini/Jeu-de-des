1b04e4aed7740bc3b547ad6c0de63eb932a2df70
Aajdini <elv-artrit.ajdn@eduge.ch>
2017-05-12 10:28:48 +0200

    Modification structure de code

--- src/main/java/com/ajdini/hesge/App.java
+++ src/main/java/com/ajdini/hesge/App.java
@@ -9,38 +9,30 @@ import java.util.ArrayList;
 public class App 
 {
     
-    private static int[] table  = new int[3];
-    private static int shots = 0;
+    private  int[] table  = new int[3];
+    private  int shots = 0;
     
-    public static void main( String[] args )
-    {
-        System.out.println( "1er lancé" );
-        lancerDes();
-        checkValues();
-
+    public void jouerEssais(){}
+   
+    public void lancerDes(){
+        shots++;
+        table[0] = getShotValue();
+        table[1] = getShotValue();
+        table[2] = getShotValue();
     }
     
-    
-    public static int getShotValue(){
+    public int getShotValue(){
         return (int)(Math.random() * 6);
     }
     
-    public static boolean checkValues(){
-        for(int i = 0; i<table.length-1;i++){
-            if(table[i] == 1  || table[i] == 2 || table[i] == 4 ){  
-                System.out.println("Trouvé");
-                System.out.println(table[i]);
-            }
-            return false;
+    public boolean checkValues(){
+        for(int i = 0; i<table.length-1;i++){         
+           
         }
         table = null;
         System.out.println("C'est raté");
         return false;
     }
     
-    public static void lancerDes(){
-        table[0] = getShotValue();
-        table[1] = getShotValue();
-        table[2] = getShotValue();
-    }
+
 }
--- src/main/java/com/ajdini/hesge/Main.java
+++ src/main/java/com/ajdini/hesge/Main.java
@@ -5,10 +5,14 @@
  */
 package com.ajdini.hesge;
 
+
 /**
  *
  * @author Artrit
  */
 public class Main {
-    
+    public static void main( String[] args )
+    {
+        
+    }
 }
